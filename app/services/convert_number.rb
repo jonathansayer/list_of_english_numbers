130ff29a0c76f738bf08e8d736c7b021fa22d4f0
jonathansayer <jon.sayer1@gmail.com>
2015-11-12 22:59:32 +0000

    three digit numbers can be converted

--- app/services/convert_number.rb
+++ app/services/convert_number.rb
@@ -1,54 +1,41 @@
 class Convert_Number
 
-@numbers = { 1 => 'one',
-             2 => 'two',
-             3 => 'three',
-             4 => 'four',
-             5 => 'five',
-             6 => 'six',
-             7 => 'seven',
-             8 =>  'eight',
-             9 => 'nine'}
+@numbers = { '1' => 'one',
+             '2' => 'two',
+             '3' => 'three',
+             '4' => 'four',
+             '5'=> 'five',
+             '6' => 'six',
+             '7' => 'seven',
+             '8' => 'eight',
+             '9' => 'nine'}
 
-@tens = [ 'ten',
-          'twenty',
-          'thirty',
-          'forty',
-          'fifty',
-          'sixty',
-          'seventy',
-          'eighty',
-          'ninety']
+@tens = { '1' => 'ten',
+          '2' => 'twenty',
+          '3' => 'thirty',
+          '4' => 'forty',
+          '5' => 'fifty',
+          '6' => 'sixty',
+          '7' => 'seventy',
+          '8' => 'eighty',
+          '9' => 'ninety'}
 
-@teens = [ 'eleven',
-           'twelve',
-           'thirteen',
-           'fourteen',
-           'fifteen',
-           'sixteen',
-           'seventeen',
-           'eighteen',
-           'nineteen']
-
-  @big_numbers = [ [2, 'hundred'],
-                 [3, 'thousand'],
-                 [6, 'million'],
-                 [9, 'billion']]
+@teens = { '11' => 'eleven',
+           '12' => 'twelve',
+           '13' => 'thirteen',
+           '14' => 'fourteen',
+           '15' => 'fifteen',
+           '16' => 'sixteen',
+           '17' => 'seventeen',
+           '18' => 'eighteen',
+           '19' => 'nineteen'}
 
   def self.convert number
-    big_pair = @big_numbers.pop
-    prefix = big_pair[1]
-    power = 10 ** big_pair[0]
-    number = number/power
-    num_string = @numbers[number] +" " +  prefix
+    number = number.to_s
+    return @numbers[number] if number.length == 1
+    return self.tens number if number.length == 2
+    return self.hundreds number if number.length == 3
   end
-
-  # def self.convert number
-  #   number = number.to_s
-  #   return @numbers[number] if number.length == 1
-  #   return self.tens number if number.length == 2
-  #   return self.hundreds number if number.length == 3
-  # end
 
   private
 
