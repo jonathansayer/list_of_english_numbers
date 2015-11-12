e490be3949a3da54f62f36345cc6f458b6501c16
jonathansayer <jon.sayer1@gmail.com>
2015-11-12 22:49:45 +0000

    can convert one billion to words

--- app/services/convert_number.rb
+++ app/services/convert_number.rb
@@ -1,41 +1,54 @@
-class ConvertNumber
+class Convert_Number
 
-@numbers = { '1' => 'one',
-             '2' => 'two',
-             '3' => 'three',
-             '4' => 'four',
-             '5'=> 'five',
-             '6' => 'six',
-             '7' => 'seven',
-             '8' => 'eight',
-             '9' => 'nine'}
+@numbers = { 1 => 'one',
+             2 => 'two',
+             3 => 'three',
+             4 => 'four',
+             5 => 'five',
+             6 => 'six',
+             7 => 'seven',
+             8 =>  'eight',
+             9 => 'nine'}
 
-@tens = { '1' => 'ten',
-          '2' => 'twenty',
-          '3' => 'thirty',
-          '4' => 'forty',
-          '5' => 'fifty',
-          '6' => 'sixty',
-          '7' => 'seventy',
-          '8' => 'eighty',
-          '9' => 'ninety'}
+@tens = [ 'ten',
+          'twenty',
+          'thirty',
+          'forty',
+          'fifty',
+          'sixty',
+          'seventy',
+          'eighty',
+          'ninety']
 
-@teens = { '11' => 'eleven',
-           '12' => 'twelve',
-           '13' => 'thirteen',
-           '14' => 'fourteen',
-           '15' => 'fifteen',
-           '16' => 'sixteen',
-           '17' => 'seventeen',
-           '18' => 'eighteen',
-           '19' => 'nineteen'}
+@teens = [ 'eleven',
+           'twelve',
+           'thirteen',
+           'fourteen',
+           'fifteen',
+           'sixteen',
+           'seventeen',
+           'eighteen',
+           'nineteen']
+
+  @big_numbers = [ [2, 'hundred'],
+                 [3, 'thousand'],
+                 [6, 'million'],
+                 [9, 'billion']]
 
   def self.convert number
-    number = number.to_s
-    return @numbers[number] if number.length == 1
-    return self.tens number if number.length == 2
-    return self.hundreds number if number.length == 3
+    big_pair = @big_numbers.pop
+    prefix = big_pair[1]
+    power = 10 ** big_pair[0]
+    number = number/power
+    num_string = @numbers[number] +" " +  prefix
   end
+
+  # def self.convert number
+  #   number = number.to_s
+  #   return @numbers[number] if number.length == 1
+  #   return self.tens number if number.length == 2
+  #   return self.hundreds number if number.length == 3
+  # end
 
   private
 
--- spec/services/convert_number_spec.rb
+++ spec/services/convert_number_spec.rb
@@ -1,83 +1,87 @@
 require 'rails_helper'
 
-describe ConvertNumber do
-  let(:subject){ConvertNumber}
+describe Convert_Number do
+  let(:subject){Convert_Number}
 
   it'should take a number and convert it words' do
     expect(subject).to respond_to(:convert).with(1).argument
   end
 
-  context 'converting single digit numbers' do
+  # context 'converting single digit numbers' do
+  #
+  #   it'should convert the number 1 to the word one' do
+  #     expect(subject.convert 1).to eq 'one'
+  #   end
+  #
+  #   it'should convert the number 2 to the word two' do
+  #     expect(subject.convert 2).to eq 'two'
+  #   end
+  #
+  #   it'should cconvert the number 8 to the word "eigth"' do
+  #     expect(subject.convert 8).to eq 'eight'
+  #   end
+  # end
+  #
+  # context 'converting double digit numbers' do
+  #
+  #   it'should convert the number 10 to the word "ten"' do
+  #     expect(subject.convert 10).to eq 'ten'
+  #   end
+  #
+  #   it 'should convert the number 20 to the word "twenty"' do
+  #     expect(subject.convert 20).to eq 'twenty'
+  #   end
+  #
+  #   it 'should convert the number 70 to the word "seventy"' do
+  #     expect(subject.convert 70).to eq 'seventy'
+  #   end
+  #
+  #   it 'should convert the number 21 to the word "twenty-one"' do
+  #     expect(subject.convert 21).to eq "twenty one"
+  #   end
+  #
+  #   it 'should convert the number 46 to the word "forty-six"' do
+  #     expect(subject.convert 46).to eq "forty six"
+  #   end
+  #
+  #   it 'should convert the number 99 to the word "ninety-nine"' do
+  #     expect(subject.convert 99).to eq "ninety nine"
+  #   end
+  # end
+  #
+  # context 'converting teen numbers' do
+  #
+  #   it 'should convert the number 11 to the word "eleven"' do
+  #     expect(subject.convert 11).to eq 'eleven'
+  #   end
+  #
+  #   it 'should convert the number 13 to the word "thirteen"' do
+  #     expect(subject.convert 13).to eq 'thirteen'
+  #   end
+  #
+  #   it 'should convert the number 19 to the word "nineteen"' do
+  #     expect(subject.convert 19).to eq 'nineteen'
+  #   end
+  #
+  # end
+  #
+  # context 'converting three digit numbers' do
+  #
+  #   it'should convert the number 100 to the word "one-hundred"' do
+  #     expect(subject.convert 100).to eq 'one hundred'
+  #   end
+  #
+  #   it 'should convert the number 110 to the words "one hundred and ten"' do
+  #     expect(subject.convert 110).to eq 'one hundred and ten'
+  #   end
+  #
+  #   it 'should convert the number 240 to the words "two hundred and forty"' do
+  #     expect(subject.convert 240).to eq 'two hundred and forty'
+  #   end
+  # end
 
-    it'should convert the number 1 to the word one' do
-      expect(subject.convert 1).to eq 'one'
-    end
-
-    it'should convert the number 2 to the word two' do
-      expect(subject.convert 2).to eq 'two'
-    end
-
-    it'should cconvert the number 8 to the word "eigth"' do
-      expect(subject.convert 8).to eq 'eight'
-    end
-  end
-
-  context 'converting double digit numbers' do
-
-    it'should convert the number 10 to the word "ten"' do
-      expect(subject.convert 10).to eq 'ten'
-    end
-
-    it 'should convert the number 20 to the word "twenty"' do
-      expect(subject.convert 20).to eq 'twenty'
-    end
-
-    it 'should convert the number 70 to the word "seventy"' do
-      expect(subject.convert 70).to eq 'seventy'
-    end
-
-    it 'should convert the number 21 to the word "twenty-one"' do
-      expect(subject.convert 21).to eq "twenty one"
-    end
-
-    it 'should convert the number 46 to the word "forty-six"' do
-      expect(subject.convert 46).to eq "forty six"
-    end
-
-    it 'should convert the number 99 to the word "ninety-nine"' do
-      expect(subject.convert 99).to eq "ninety nine"
-    end
-  end
-
-  context 'converting teen numbers' do
-
-    it 'should convert the number 11 to the word "eleven"' do
-      expect(subject.convert 11).to eq 'eleven'
-    end
-
-    it 'should convert the number 13 to the word "thirteen"' do
-      expect(subject.convert 13).to eq 'thirteen'
-    end
-
-    it 'should convert the number 19 to the word "nineteen"' do
-      expect(subject.convert 19).to eq 'nineteen'
-    end
-
-  end
-
-  context 'converting three digit numbers' do
-
-    it'should convert the number 100 to the word "one-hundred"' do
-      expect(subject.convert 100).to eq 'one hundred'
-    end
-
-    it 'should convert the number 110 to the words "one hundred and ten"' do
-      expect(subject.convert 110).to eq 'one hundred and ten'
-    end
-
-    it 'should convert the number 240 to the words "two hundred and forty"' do
-      expect(subject.convert 240).to eq 'two hundred and forty'
-    end
+  it'should convert 1000000000 to the word one billion' do
+    expect(subject.convert 1000000000).to eq "one billion"
   end
 
 end
