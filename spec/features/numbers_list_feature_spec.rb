fb4c3119bfc97495c64f66b1853b0f56379ffa1a
jonathansayer <jon.sayer1@gmail.com>
2015-11-13 11:20:07 +0000

    refactored test

--- spec/features/numbers_list_feature_spec.rb
+++ spec/features/numbers_list_feature_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 feature 'numbers list' do
+
+  before(:each) do
+    visit '/numbers'
+    fill_in("Number", :with => "5")
+  end 
+
   context'selecting the number you want and the numbers per page' do
     scenario 'should display a prompt to enter a number to generate list' do
       visit '/numbers'
@@ -8,8 +14,6 @@ feature 'numbers list' do
     end
 
     scenario 'should allow a user to enter a number a receive a list' do
-      visit '/numbers'
-      fill_in("Number", :with => "5")
       click_button "Generate List"
       expect(page).to have_content 'five'
       expect(page).to have_content 'four'
@@ -19,8 +23,6 @@ feature 'numbers list' do
     end
 
     scenario 'should allow a user to enter a number of pages' do
-      visit '/numbers'
-      fill_in("Number", :with => "5")
       fill_in("number_per_page", :with => 3)
       click_button "Generate List"
       expect(page).to have_content 'three'
