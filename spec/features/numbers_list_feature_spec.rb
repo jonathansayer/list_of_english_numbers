d8e43055ec74a43b883f14eb2970026acce04041
jonathansayer <jon.sayer1@gmail.com>
2015-12-06 14:04:09 +0000

    added better errors

--- spec/features/numbers_list_feature_spec.rb
+++ spec/features/numbers_list_feature_spec.rb
@@ -2,12 +2,13 @@ require "rails_helper"
 
 feature 'numbers list' do
 
-  before(:each) do
-    visit '/numbers'
-    fill_in("Number", :with => "5")
-  end
-
   context'selecting the number you want and the numbers per page' do
+
+    before(:each) do
+      visit '/numbers'
+      fill_in("Number", :with => "5")
+    end
+
     scenario 'should display a prompt to enter a number to generate list' do
       visit '/numbers'
       expect(page).to have_content "Number: "
@@ -36,4 +37,20 @@ feature 'numbers list' do
       expect(page).to_not have_content 'six'
     end
   end
+
+  context 'large numbers' do
+
+    scenario 'when selecting large numbers' do
+      visit '/'
+      fill_in("Number", :with => "101")
+      fill_in("number_per_page", :with => 101)
+      click_button "Generate List"
+      expect(page).to have_content 'one hundred and one'
+      expect(page).to have_content 'one hundred'
+      expect(page).to have_content 'ninety nine'
+      expect(page).to have_content 'ninety eigth'
+      expect(page).to have_content 'ninety seven'
+    end
+  end
+
 end
