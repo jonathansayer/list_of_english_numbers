3a397df61357861d882ab1fbfd86974f94e7b309
jonathansayer <jon.sayer1@gmail.com>
2015-12-06 18:34:21 +0000

    removed redundant returns

--- app/services/convert_number.rb
+++ app/services/convert_number.rb
@@ -37,7 +37,7 @@ class Convert_Number
     return self.tens number if number.length == 2
     return self.hundreds number if number.length == 3
     return self.thousands number if number.length == 4
-    return self.hundreds_of_thousands number if number.length > 4
+    self.hundreds_of_thousands number if number.length > 4
   end
 
   private
@@ -47,30 +47,30 @@ class Convert_Number
     number_of_thousands = split_number.reverse.drop(1).reverse.join('')
     thousands = self.convert number_of_thousands
     hundreds = self.convert split_number.last
-    return thousands + ' thousand ' + hundreds
+    thousands + ' thousand ' + hundreds
   end
 
   def self.thousands number
     return @numbers[number[0]] + ' thousand' if number[1] + number[2] + number[3] == '000'
-    return @numbers[number[0]] + ' thousand and ' + self.hundreds(number[1] + number[2] + number[3])
+    @numbers[number[0]] + ' thousand and ' + self.hundreds(number[1] + number[2] + number[3])
   end
 
   def self.hundreds number
     return '' if number[0] == '0'
     return @numbers[number[0]] + ' hundred' if number[1] == '0' and number[2] == '0'
-    return @numbers[number[0]] + ' hundred and ' + @numbers[number[2]] if number[1] == '0' 
-    return @numbers[number[0]] + ' hundred and ' + self.tens(number[1] + number[2])
+    return @numbers[number[0]] + ' hundred and ' + @numbers[number[2]] if number[1] == '0'
+    @numbers[number[0]] + ' hundred and ' + self.tens(number[1] + number[2])
   end
 
   def self.tens number
     return @tens[number[0]] if number.length == 2 and number[1] == '0'
     return @teens[number] if self.is_teen? number
-    return (@tens[number[0]] + " " + @numbers[number[1]]) if number.length == 2 and !self.is_teen? number
+    (@tens[number[0]] + " " + @numbers[number[1]]) if number.length == 2 and !self.is_teen? number
   end
 
   def self.is_teen? number
     return true if number.length == 2 and number[0] == '1'
-    return false
+    false
   end
 
   def self.below_ten? number
